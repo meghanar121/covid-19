e7a23f69aea13ca945347ae361aa567b06fb8bb5
meghanar121 <meghanar121@gmail.com>
2020-06-06 17:03:11 +0200

    Updates

---
+++ precache-manifest.0e53996849b142e4be6af864d32ffd52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03000df4d91c736e153762b19ffc29e5",
+    "url": "/meghanar121/covid-19/index.html"
+  },
+  {
+    "revision": "c49d4e4e437c79c17d39",
+    "url": "/meghanar121/covid-19/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9266fd8efc543f3b9bc0",
+    "url": "/meghanar121/covid-19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c49d4e4e437c79c17d39",
+    "url": "/meghanar121/covid-19/static/js/2.bab46453.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/meghanar121/covid-19/static/js/2.bab46453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9266fd8efc543f3b9bc0",
+    "url": "/meghanar121/covid-19/static/js/main.c4b69c2c.chunk.js"
+  },
+  {
+    "revision": "db6ecf109daeae2fa775",
+    "url": "/meghanar121/covid-19/static/js/runtime-main.d817f11b.js"
+  }
+]);
